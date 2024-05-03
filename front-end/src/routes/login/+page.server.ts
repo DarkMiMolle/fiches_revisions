6dc7d06da61a9786e14c69e23d3a09c27264d442
Florent Carrez <florent.carrez@epita.fr>
2024-05-03 17:16:34 +0200

    feat(fe/login): update actions for login/signup

--- front-end/src/routes/login/+page.server.ts
+++ front-end/src/routes/login/+page.server.ts
@@ -1,5 +1,8 @@
 import { env } from '$env/dynamic/public';
-import { redirect } from '@sveltejs/kit';
+import type { ServerError } from '$lib';
+import { fail, redirect } from '@sveltejs/kit';
+
+const backendUrl = env.PUBLIC_BACKEND
 
 export async function load({parent, cookies}) {
 	await parent()
@@ -9,23 +12,55 @@ export async function load({parent, cookies}) {
 }
 
 export const actions = {
-	default: async ({ request, cookies, url }) => {
+	login: async ({ request, cookies, url }) => {
 		const data = await request.formData()
-		const backendUrl = env.PUBLIC_BACKEND
+		const pseudo = data.get("pseudo") as string, password = data.get("password") as string
+		const body = {pseudo, password}
+		// const errors = []
+
+		// if (!/^([a-zA-Z]+[a-zA-Z0-9_-]*)$/.test(pseudo) || pseudo.length < 6) {
+		// 	errors.push({pseudo: `${pseudo} n'est pas valide; un pseudo doit commencer par une lettre, doit avoir au moins 6 caractère parmis: des lettres, des chiffres, '-' ou '_'`})
+		// }
+
+		// if (password.length < 7) {
+		// 	errors.push({password: "un mot de passe doit avoir au moins 7 caractères"})
+		// }
+		
 		console.log(backendUrl)
 		const resp = await fetch(`${backendUrl}/api/login`, {
 			method: "POST",
-			body: JSON.stringify({
-				email: "florent.carrez@yahoo.fr",
-				password: 'passw0rd'
-			})
+			body: JSON.stringify(body)
 		})
 		const result = await resp.json()
 		console.log(result)
 		if (!resp.ok) {
-			return
+			return fail(resp.status, result)
 		}
-		cookies.set('jwt', result["jwt"], { path: '/' });
-		throw redirect(303, url.searchParams.get('redirectTo') ?? '/');
+		cookies.set('jwt', result.jwt, { path: '/' });
+		return redirect(303, url.searchParams.get('redirectTo') ?? '/');
+	},
+
+	signup: async ({request, cookies, url}) => {
+		const data = await request.formData()
+		const pseudo = data.get("pseudo") as string, password = data.get("password") as string,
+			email =	data.get("email") as string, notif = data.get("notif") as string == "true"
+
+		const body = {pseudo, password, email, notif}
+		const resp = await fetch(`${backendUrl}/api/signup`, {
+			method: "POST",
+			body: JSON.stringify(body)
+		})
+		const result = await resp.json()
+		console.log(result)
+		if (!resp.ok) {
+			return fail(resp.status, result)
+		}
+		cookies.set("jwt", result.jwt, {path: "/"})
+		console.log(url.origin)
+		// return redirect(303, url.searchParams.get('redirectTo') ?? '/');
+		return fail(501, {
+			msg: "TODO"
+		})
+
 	}
 };
