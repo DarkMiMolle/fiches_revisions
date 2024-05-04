6620e3b06951f33e3a757bc2f98b9907163ff9e8
Florent Carrez <florent.carrez@epita.fr>
2024-05-04 18:41:12 +0200

    feat(be/models): update form model

--- back-end/models/form.go
+++ back-end/models/form.go
@@ -1,6 +1,26 @@
 package models
 
+import (
+	"fmt"
+	"time"
+)
+
+type Level int
+
+type Percent float64
+
+func (pc Percent) Of(other Percent) Percent {
+	return (pc * other) / 10_000
+}
+func (pc Percent) String() string {
+	return fmt.Sprintf("%.2f %%", float64(pc))
+}
+
 type Form struct {
-	Question string   `json:"question" bson:"question"`
-	Answers  []string `json:"answers" bson:"answers"`
+	Question       string    `json:"question" bson:"question"`
+	Answers        []string  `json:"answers" bson:"answers"`
+	Tips           []string  `json:"tips" bson:"tips"`
+	Level          Level     `json:"level" bson:"level"`
+	LastDateWorked time.Time `json:"lastDateWorked" bson:"lastDateWorked"`
+	SuccessRate    Percent   `json:"successRate"`
 }
