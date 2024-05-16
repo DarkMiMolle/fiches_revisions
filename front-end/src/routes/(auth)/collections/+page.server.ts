1032fe761d9f6be5e183498a17f6f8f752b705f9
Florent Carrez <florent.carrez@epita.fr>
2024-05-16 16:49:21 +0200

    test(fe): add logs to see jwt token

--- front-end/src/routes/(auth)/collections/+page.server.ts
+++ front-end/src/routes/(auth)/collections/+page.server.ts
@@ -4,11 +4,14 @@ import { HttpCode } from '$lib/statusCode.js'
 import { CollectionsTest } from '$lib/test.js'
 import { redirect } from '@sveltejs/kit'
 
+const __filename = "collection.server"
 
 export async function load({cookies, parent, url}) {
     await parent()
     const jwt = cookies.get("jwt")!
     const endpoint = env.PUBLIC_BACKEND
+
+    console.log(__filename, {jwt})
 
     const fetchResp = fetch(`${endpoint}/api/collections`, {
         method: "GET",
@@ -27,7 +30,7 @@ export async function load({cookies, parent, url}) {
             const result = await resp.json()
             if (!resp.ok) {
                 if ((result as ServerError).status === HttpCode.UNAUTHORIZED) {
-                    console.log("collection.server", {result})
+                    console.log(__filename, {result})
                     cookies.delete("jwt", {path: '/'})
                     throw redirect(HttpCode.TEMPORARY_REDIRECT, `/login?redirectTo=${url.pathname}`)
                 }
